a73da1ac731a899a815c41eae6ec73e30c21827d
Nikita Kornilov <n.b.kornilov@gmail.com>
2015-10-29 18:04:29 +0300

    FIX event handler

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -3,19 +3,22 @@
 //var dropdownForFirst = new EJS({url: 'templates/dropdown_for_first.html.ejs'}).render({ collections: collections });
 
 $(document).ready(function() {
-    $('.dropdown > a').on("click", function (event) {
+    $('.dropdown').on("click", '.js-toggle-dropdown-item', function (event) {
         $.each(collections, function (key, value){
             if($(event.target).data("type") == key && $("." + key).children("li").length == 0)
                     appendDropdownFirst(key);
         });
     });
 
-    $('.dropdown-menu > li').on("click", function (event) {
-        console.log("something");
-    })
+    $(".dropdown-menu").on('click', 'li', function(e) {
+      console.log(" WORKS ")
+    });
+
+
 });
 function appendDropdownFirst(typeItem){
     $.each(collections[typeItem], function (i,value){
         $("." + typeItem).append("<li><a data-type = '" + typeItem + "' data-id='" + collections[typeItem][i].id + "'>" + collections[typeItem][i].name + "</a></li>");
-    })
+    });
+
 }
