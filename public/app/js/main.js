659a02e85ad1ebc777f8c4168d8a0bdfe7e33ab3
baluevD <newarmlet@gmail.com>
2015-11-01 19:58:03 +0300

    fill summary 1 row

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -13,17 +13,48 @@ $(document).ready(function() {
     $('.dropdown-menu').on("click", 'li', function (event) {
         var arrChildItem = findChild(event);
         var keyForTemp = $(event.target).data("type");
-        tempItem[keyForTemp]=$(this).text();
+        tempItem[keyForTemp] = $(this).text();
+        if($('.summary').children("tr").length == 0){
+            putTr();
+            //
+            //$('.summary tr:last').append("<td><input type='text' class=" + keyForTemp + "" +
+            //    " value=" + tempItem[keyForTemp] + "></td><td><input type='text' class='brand'></td>" +
+            //    "<td><input type='text' class='material'></td>");
+        }
+        if(!$('tr:last > td > input').hasClass(keyForTemp)){
+            $('tr:last').append("<td><input type='text' class=" + keyForTemp + "" +
+                " value=" + tempItem[keyForTemp] + "></td>");
+        }
+        else{
+            $('input.' + keyForTemp).val(tempItem[keyForTemp]);
+        }
         appendDropdownChild(getTypeOfItem(arrChildItem),arrChildItem);
-    })
-
+    });
+    $('.btnRemove').on("click", function(){
+        //var checked = $('input:checkbox:checked').map(function () {
+        //    return this.value;
+        //}).get();
+        $('input:checkbox:checked').parents("tr").remove();
+    });
+    $('.btnAdd').on("click", function(){
+        putTr();
+        $('.summary tr:last').append("<td><input type='text' class=" + keyForTemp + "" +
+            " value=" + tempItem[keyForTemp] + "></td><td><input type='text' class='brand'></td>" +
+            "<td><input type='text' class='material'></td>");
+    });
 });
+
 function appendDropdownFirst(typeItem){
     $.each(collections[typeItem], function (i,value){
         $("." + typeItem).append("<li><a data-type = '" + typeItem + "' data-id='"
             + collections[typeItem][i].id + "'>"
             + collections[typeItem][i].name + "</a></li>");
     })
+}
+
+function putTr(){
+    $('.summary').append("<tr></tr>");
+    $('.summary tr:last').append("<td><input type='checkbox'></td>");
 }
 
 function findChild(parent){
@@ -42,6 +73,8 @@ function appendDropdownChild(typeItem,arrChildItem){
     })
 }
 
+var arrItem=[];
+
 var tempItem ={};
 
 var items = [
@@ -50,49 +83,59 @@ var items = [
         label:"Крыша",
         value: "roof",
         pid: 0,
-        id: '1'
+        id: 1
     },
     {
         type:"brand",
         label:"Руфлекс",
         value: "ruflex",
         pid: 1,
-        id: '2'
+        id: 2
     },
     {
         type:"material",
         label:"Андулин",
+        value: "ondulin",
+        cost: 100,
         pid: 2,
-        id: '3'
+        id: 3
     },
     {
         type: "material",
         label: "Шифер",
+        value: "slate",
+        cost: 110,
         pid: 2,
-        id: '4'
+        id: 4
     },
     {
         type: "material",
         label: "Черепица",
+        value: "tile",
+        cost: 120,
         pid: 2,
-        id: '5'
+        id: 5
     },
     {
         type: "brand",
         label: "ИКО",
-        value: "ico",
+        value: "iko",
         pid: 1,
-        id: '6'
+        id: 6
     },
     {
         type: "material",
         label: "Андулин",
+        value: "ondulin",
+        cost: 140,
         pid: 6,
         id: 7
     },
     {
         type: "material",
         label: "Черепица",
+        value: "tile",
+        cost: 150,
         pid: 6,
         id: 8
     },
@@ -106,99 +149,127 @@ var items = [
     {
         type: "brand",
         label: "ИКО",
+        value: "iko",
         pid: 9,
         id: 10
     },
     {
         type: "material",
         label: "Кирпич",
+        value: "brick",
+        cost: 160,
         pid: 10,
         id: 11
     },
     {
         type: "material",
         label: "Дерево",
+        value: "timber",
+        cost: 170,
         pid: 10,
         id: 12
     },
     {
         type: "category",
         label: "Фундамент",
+        value: "basement",
         pid: 0,
         id: 13
     },
     {
         type: "brand",
         label: "Руфлекс",
+        value: "ruflex",
         pid: 13,
         id: 14
     },
     {
         type:"material",
         label:"Бетон",
+        value:"concrete",
+        cost: 180,
         pid: 14,
         id: 15
     },
     {
         type: "brand",
         label: "ИКО",
+        value:"iko",
         pid: 13,
         id: 16
     },
     {
         type: "material",
         label: "Бетон",
+        value:"concrete",
+        cost: 190,
         pid: 16,
         id: 17
     },
     {
         type: "material",
         label: "Камень",
+        value:"stone",
+        cost: 200,
         pid: 16,
         id: 18
     },
     {
         type: "brand",
         label: "КБЕ",
+        value:"kbe",
         pid: 13,
         id: 19
     },
     {
         type: "material",
         label: "Лента",
+        value: "tape",
+        cost: 210,
         pid: 19,
         id: 20
     },
     {
         type: "material",
         label: "Бетон",
+        value: "concrete",
+        cost: 220,
         pid: 19,
         id: 21
     },
     {
         type: "brand",
         label: "Веко",
+        value: "veko",
         pid: 13,
         id: 22
     },
     {
         type: "material",
         label: "Бетон",
+        value: "concrete",
+        cost: 230,
         pid: 22,
         id: 23
     },
     {
         type: "material",
         label: "Лента",
+        value: "tape",
+        cost: 240,
         pid: 22,
         id: 24
     },
     {
         type: "material",
         label: "Камень",
+        value: "stone",
+        cost: 250,
         pid: 22,
         id: 25
     }
 ];
+
+
 
 
