3243c1d73a1ede5f4390907c8a9bce07cda2ba0a
Nikita Kornilov <n.b.kornilov@gmail.com>
2015-11-02 15:46:48 +0300

    SomeFixes

--- public/app/js/dataElementsOfHouse.js
+++ public/app/js/dataElementsOfHouse.js
@@ -1,178 +1,192 @@
 var items = [
-    {
-        type:"category",
-        label:"Крыша",
-        value: "roof",
-        pid: 0,
-        id: 1
-    },
-    {
-        type:"brand",
-        label:"Руфлекс",
-        value: "ruflex",
-        pid: 1,
-        id: 2
-    },
-    {
-        type:"material",
-        label:"Андулин",
-        value: "ondulin",
-        pid: 2,
-        id: 3
-    },
-    {
-        type: "material",
-        label: "Шифер",
-        value: "slate",
-        pid: 2,
-        id: 4
-    },
-    {
-        type: "material",
-        label: "Черепица",
-        value: "tile",
-        pid: 2,
-        id: 5
-    },
-    {
-        type: "brand",
-        label: "ИКО",
-        value: "iko",
-        pid: 1,
-        id: 6
-    },
-    {
-        type: "material",
-        label: "Андулин",
-        value: "ondulin",
-        pid: 6,
-        id: 7
-    },
-    {
-        type: "material",
-        label: "Черепица",
-        value: "tile",
-        pid: 6,
-        id: 8
-    },
-    {
-        type: "category",
-        label: "Стена",
-        value: "wall",
-        pid: 0,
-        id: 9
-    },
-    {
-        type: "brand",
-        label: "ИКО",
-        value: "iko",
-        pid: 9,
-        id: 10
-    },
-    {
-        type: "material",
-        label: "Кирпич",
-        value: "brick",
-        pid: 10,
-        id: 11
-    },
-    {
-        type: "material",
-        label: "Дерево",
-        value: "timber",
-        pid: 10,
-        id: 12
-    },
-    {
-        type: "category",
-        label: "Фундамент",
-        value: "basement",
-        pid: 0,
-        id: 13
-    },
-    {
-        type: "brand",
-        label: "Руфлекс",
-        value: "ruflex",
-        pid: 13,
-        id: 14
-    },
-    {
-        type:"material",
-        label:"Бетон",
-        value:"concrete",
-        pid: 14,
-        id: 15
-    },
-    {
-        type: "brand",
-        label: "ИКО",
-        value:"iko",
-        pid: 13,
-        id: 16
-    },
-    {
-        type: "material",
-        label: "Бетон",
-        value:"concrete",
-        pid: 16,
-        id: 17
-    },
-    {
-        type: "material",
-        label: "Камень",
-        value:"stone",
-        pid: 16,
-        id: 18
-    },
-    {
-        type: "brand",
-        label: "КБЕ",
-        value:"kbe",
-        pid: 13,
-        id: 19
-    },
-    {
-        type: "material",
-        label: "Лента",
-        value: "tape",
-        pid: 19,
-        id: 20
-    },
-    {
-        type: "material",
-        label: "Бетон",
-        value: "concrete",
-        pid: 19,
-        id: 21
-    },
-    {
-        type: "brand",
-        label: "Веко",
-        value: "veko",
-        pid: 13,
-        id: 22
-    },
-    {
-        type: "material",
-        label: "Бетон",
-        value: "concrete",
-        pid: 22,
-        id: 23
-    },
-    {
-        type: "material",
-        label: "Лента",
-        value: "tape",
-        pid: 22,
-        id: 24
-    },
-    {
-        type: "material",
-        label: "Камень",
-        value: "stone",
-        pid: 22,
-        id: 25
-    }
+  {
+    type:"category",
+    label:"Крыша",
+    value: "roof",
+    pid: 0,
+    id: 1
+  },
+  {
+    type:"brand",
+    label:"Руфлекс",
+    value: "ruflex",
+    pid: 1,
+    id: 2
+  },
+  {
+    type:"material",
+    label:"Андулин",
+    value: "ondulin",
+    cost: 100,
+    pid: 2,
+    id: 3
+  },
+  {
+    type: "material",
+    label: "Шифер",
+    value: "slate",
+    cost: 110,
+    pid: 2,
+    id: 4
+  },
+  {
+    type: "material",
+    label: "Черепица",
+    value: "tile",
+    cost: 120,
+    pid: 2,
+    id: 5
+  },
+  {
+    type: "brand",
+    label: "ИКО",
+    value: "iko",
+    pid: 1,
+    id: 6
+  },
+  {
+    type: "material",
+    label: "Андулин",
+    value: "ondulin",
+    cost: 140,
+    pid: 6,
+    id: 7
+  },
+  {
+    type: "material",
+    label: "Черепица",
+    value: "tile",
+    cost: 150,
+    pid: 6,
+    id: 8
+  },
+  {
+    type: "category",
+    label: "Стена",
+    value: "wall",
+    pid: 0,
+    id: 9
+  },
+  {
+    type: "brand",
+    label: "ИКО",
+    value: "iko",
+    pid: 9,
+    id: 10
+  },
+  {
+    type: "material",
+    label: "Кирпич",
+    value: "brick",
+    cost: 160,
+    pid: 10,
+    id: 11
+  },
+  {
+    type: "material",
+    label: "Дерево",
+    value: "timber",
+    cost: 170,
+    pid: 10,
+    id: 12
+  },
+  {
+    type: "category",
+    label: "Фундамент",
+    value: "basement",
+    pid: 0,
+    id: 13
+  },
+  {
+    type: "brand",
+    label: "Руфлекс",
+    value: "ruflex",
+    pid: 13,
+    id: 14
+  },
+  {
+    type:"material",
+    label:"Бетон",
+    value:"concrete",
+    cost: 180,
+    pid: 14,
+    id: 15
+  },
+  {
+    type: "brand",
+    label: "ИКО",
+    value:"iko",
+    pid: 13,
+    id: 16
+  },
+  {
+    type: "material",
+    label: "Бетон",
+    value:"concrete",
+    cost: 190,
+    pid: 16,
+    id: 17
+  },
+  {
+    type: "material",
+    label: "Камень",
+    value:"stone",
+    cost: 200,
+    pid: 16,
+    id: 18
+  },
+  {
+    type: "brand",
+    label: "КБЕ",
+    value:"kbe",
+    pid: 13,
+    id: 19
+  },
+  {
+    type: "material",
+    label: "Лента",
+    value: "tape",
+    cost: 210,
+    pid: 19,
+    id: 20
+  },
+  {
+    type: "material",
+    label: "Бетон",
+    value: "concrete",
+    cost: 220,
+    pid: 19,
+    id: 21
+  },
+  {
+    type: "brand",
+    label: "Веко",
+    value: "veko",
+    pid: 13,
+    id: 22
+  },
+  {
+    type: "material",
+    label: "Бетон",
+    value: "concrete",
+    cost: 230,
+    pid: 22,
+    id: 23
+  },
+  {
+    type: "material",
+    label: "Лента",
+    value: "tape",
+    cost: 240,
+    pid: 22,
+    id: 24
+  },
+  {
+    type: "material",
+    label: "Камень",
+    value: "stone",
+    cost: 250,
+    pid: 22,
+    id: 25
+  }
 ];
-
--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,7 +1,3 @@
-//var dropdownBodyTempl = new EJS({url: 'templates/dropdown_body.html.ejs'}).render({ items: items });
-//$(".materials").append(dropdownBodyTempl);
-//var dropdownForFirst = new EJS({url: 'templates/dropdown_for_first.html.ejs'}).render({ collections: collections });
-
 $(document).ready(function() {
     dropdownToggleItem();
     dropdownMenuLiClick();
@@ -9,13 +5,13 @@ $(document).ready(function() {
     removeChosenRows();
 });
 
-function dropdownToggleItem(){
+function dropdownToggleItem() {
     $('.dropdown').on("click", '.js-toggle-dropdown-item', function (event) {
-    $.each(collections, function (key, value){
-        if($(event.target).data("type") == key && $("." + key).children("li").length == 0)
-            appendDropdownFirst(key);
+      $.each(collections, function (key, value){
+          if($(event.target).data("type") == key && $("." + key).children("li").length == 0)
+              appendDropdownFirst(key);
+      });
     });
-});
 }
 
 function dropdownMenuLiClick(){
@@ -112,7 +108,7 @@ function checkData(){
         alert("Неверный ввод производителя или материала.");
         return false;
     }
-    else{
+    else {
         if(dataInRow['brand'].pid != dataInRow['category'].id){
             alert("Неверный ввод производителя или категории.");
             return false;
@@ -136,198 +132,6 @@ var tempItems = [];
 
 var tempItem = {};
 
-var items = [
-    {
-        type:"category",
-        label:"Крыша",
-        value: "roof",
-        pid: 0,
-        id: 1
-    },
-    {
-        type:"brand",
-        label:"Руфлекс",
-        value: "ruflex",
-        pid: 1,
-        id: 2
-    },
-    {
-        type:"material",
-        label:"Андулин",
-        value: "ondulin",
-        cost: 100,
-        pid: 2,
-        id: 3
-    },
-    {
-        type: "material",
-        label: "Шифер",
-        value: "slate",
-        cost: 110,
-        pid: 2,
-        id: 4
-    },
-    {
-        type: "material",
-        label: "Черепица",
-        value: "tile",
-        cost: 120,
-        pid: 2,
-        id: 5
-    },
-    {
-        type: "brand",
-        label: "ИКО",
-        value: "iko",
-        pid: 1,
-        id: 6
-    },
-    {
-        type: "material",
-        label: "Андулин",
-        value: "ondulin",
-        cost: 140,
-        pid: 6,
-        id: 7
-    },
-    {
-        type: "material",
-        label: "Черепица",
-        value: "tile",
-        cost: 150,
-        pid: 6,
-        id: 8
-    },
-    {
-        type: "category",
-        label: "Стена",
-        value: "wall",
-        pid: 0,
-        id: 9
-    },
-    {
-        type: "brand",
-        label: "ИКО",
-        value: "iko",
-        pid: 9,
-        id: 10
-    },
-    {
-        type: "material",
-        label: "Кирпич",
-        value: "brick",
-        cost: 160,
-        pid: 10,
-        id: 11
-    },
-    {
-        type: "material",
-        label: "Дерево",
-        value: "timber",
-        cost: 170,
-        pid: 10,
-        id: 12
-    },
-    {
-        type: "category",
-        label: "Фундамент",
-        value: "basement",
-        pid: 0,
-        id: 13
-    },
-    {
-        type: "brand",
-        label: "Руфлекс",
-        value: "ruflex",
-        pid: 13,
-        id: 14
-    },
-    {
-        type:"material",
-        label:"Бетон",
-        value:"concrete",
-        cost: 180,
-        pid: 14,
-        id: 15
-    },
-    {
-        type: "brand",
-        label: "ИКО",
-        value:"iko",
-        pid: 13,
-        id: 16
-    },
-    {
-        type: "material",
-        label: "Бетон",
-        value:"concrete",
-        cost: 190,
-        pid: 16,
-        id: 17
-    },
-    {
-        type: "material",
-        label: "Камень",
-        value:"stone",
-        cost: 200,
-        pid: 16,
-        id: 18
-    },
-    {
-        type: "brand",
-        label: "КБЕ",
-        value:"kbe",
-        pid: 13,
-        id: 19
-    },
-    {
-        type: "material",
-        label: "Лента",
-        value: "tape",
-        cost: 210,
-        pid: 19,
-        id: 20
-    },
-    {
-        type: "material",
-        label: "Бетон",
-        value: "concrete",
-        cost: 220,
-        pid: 19,
-        id: 21
-    },
-    {
-        type: "brand",
-        label: "Веко",
-        value: "veko",
-        pid: 13,
-        id: 22
-    },
-    {
-        type: "material",
-        label: "Бетон",
-        value: "concrete",
-        cost: 230,
-        pid: 22,
-        id: 23
-    },
-    {
-        type: "material",
-        label: "Лента",
-        value: "tape",
-        cost: 240,
-        pid: 22,
-        id: 24
-    },
-    {
-        type: "material",
-        label: "Камень",
-        value: "stone",
-        cost: 250,
-        pid: 22,
-        id: 25
-    }
-];
 
 
 
