799eb48f0f8f52a107311e84ec3ffadbf5d8e303
baluevD <newarmlet@gmail.com>
2015-11-02 14:20:28 +0300

    summury ready

--- public/app/js/dataElementsForTree.js
+++ public/app/js/dataElementsForTree.js
@@ -5,5 +5,6 @@ var collections;
 collections = {
     category: [{name: "Крыша", id: 1}, {name: "Стена", id: 9}, {name: "Фундамент", id: 13}],
     brand: [{name: "Руфлекс", id: 2}, {name: "Веко", id: 5}, {name: "Ико", id: 6}],
-    material: [{name: "Андулин", id: 3}, {name: "Шифер", id: 4}, {name: "Дерево", id: 9}]
+    material: [{name: "Андулин", id: 3}, {name: "Шифер", id: 4}, {name: "Дерево", id: 9}],
+    cost: []
 };
--- public/app/js/main.js
+++ public/app/js/main.js
@@ -3,79 +3,137 @@
 //var dropdownForFirst = new EJS({url: 'templates/dropdown_for_first.html.ejs'}).render({ collections: collections });
 
 $(document).ready(function() {
-    $('.dropdown').on("click", '.js-toggle-dropdown-item', function (event) {
-        $.each(collections, function (key, value){
-            if($(event.target).data("type") == key && $("." + key).children("li").length == 0)
-                appendDropdownFirst(key);
-        });
-    });
+    dropdownToggleItem();
+    dropdownMenuLiClick();
+    addByButton();
+    removeChosenRows();
+});
 
+function dropdownToggleItem(){
+    $('.dropdown').on("click", '.js-toggle-dropdown-item', function (event) {
+    $.each(collections, function (key, value){
+        if($(event.target).data("type") == key && $("." + key).children("li").length == 0)
+            appendDropdownFirst(key);
+    });
+});
+}
+
+function dropdownMenuLiClick(){
     $('.dropdown-menu').on("click", 'li', function (event) {
         var arrChildItem = findChild(event);
         var keyForTemp = $(event.target).data("type");
+        var idForTemp = $(event.target).data("id");
         tempItem[keyForTemp] = $(this).text();
         if($('.summary').children("tr").length == 0){
             putTr();
-            //
-            //$('.summary tr:last').append("<td><input type='text' class=" + keyForTemp + "" +
-            //    " value=" + tempItem[keyForTemp] + "></td><td><input type='text' class='brand'></td>" +
-            //    "<td><input type='text' class='material'></td>");
         }
-        if(!$('tr:last > td > input').hasClass(keyForTemp)){
-            $('tr:last').append("<td><input type='text' class=" + keyForTemp + "" +
-                " value=" + tempItem[keyForTemp] + "></td>");
-        }
-        else{
-            $('input.' + keyForTemp).val(tempItem[keyForTemp]);
-        }
+        $('tr:last > td > input.' + keyForTemp).val(tempItem[keyForTemp]);
+        $('tr:last > td > input.' + keyForTemp).data('id',idForTemp);
+        $('tr:last > td > input.cost').val(findCost(keyForTemp,tempItem[keyForTemp]));
         appendDropdownChild(getTypeOfItem(arrChildItem),arrChildItem);
     });
-    $('.btnRemove').on("click", function(){
-        //var checked = $('input:checkbox:checked').map(function () {
-        //    return this.value;
-        //}).get();
-        $('input:checkbox:checked').parents("tr").remove();
-    });
-    $('.btnAdd').on("click", function(){
-        putTr();
-        $('.summary tr:last').append("<td><input type='text' class=" + keyForTemp + "" +
-            " value=" + tempItem[keyForTemp] + "></td><td><input type='text' class='brand'></td>" +
-            "<td><input type='text' class='material'></td>");
-    });
-});
-
-function appendDropdownFirst(typeItem){
-    $.each(collections[typeItem], function (i,value){
-        $("." + typeItem).append("<li><a data-type = '" + typeItem + "' data-id='"
-            + collections[typeItem][i].id + "'>"
-            + collections[typeItem][i].name + "</a></li>");
-    })
 }
 
 function putTr(){
     $('.summary').append("<tr></tr>");
-    $('.summary tr:last').append("<td><input type='checkbox'></td>");
-}
-
-function findChild(parent){
-    return _.where(items, {pid: $(parent.target).data("id")})
-}
-
-function getTypeOfItem(arrItem){
-    return arrItem[0].type;
-}
-
-function appendDropdownChild(typeItem,arrChildItem){
-    $($("." + typeItem).children("li")).remove();
-    $.each(arrChildItem, function (i,value){
-        $("." + typeItem).append("<li><a data-type = '" + typeItem + "' data-id='" + arrChildItem[i].id + "'>"
-            + arrChildItem[i].label + "</a></li>");
+    $('.summary tr:last').append("<td><input type = 'checkbox'></td>");
+    $.each(collections, function (key, value){
+        $('.summary tr:last').append("<td><input type = 'text' class = " + key + " ></td>")
     })
 }
 
-var arrItem=[];
+function addByButton(){
+    $('.btnAdd').on("click", function(){
+        if(checkData() == true){
+            gatherData(tempItems);
+            putTr();
+            dropdownMenuLiClick();
+        }
+    });
+}
 
-var tempItem ={};
+function appendDropdownFirst(typeItem){
+    $.each(collections[typeItem], function (index,value){
+        $("." + typeItem).append("<li><a data-type = " + typeItem + " data-id = "
+            + value.id + ">" + value.name + "</a></li>");
+    })
+}
+
+function appendDropdownChild(typeItem,arrChildItem){
+    if(typeItem !== undefined){
+        $($("." + typeItem).children("li")).remove();
+        $.each(arrChildItem, function (index,value){
+            $("." + typeItem).append("<li><a data-type = " + typeItem + " data-id = " + value.id + ">"
+                + value.label + "</a></li>");
+        })
+    }
+}
+
+function findChild(parent){
+    return _.where(items, {pid: $(parent.target).data("id")});
+}
+
+function findCost(typeItem,nameItem){
+    var itemForCost = _.findWhere(items, {type: typeItem, label: nameItem});
+    return itemForCost.cost;
+}
+
+function getTypeOfItem(arrItem){
+    if(arrItem[0] !== undefined){
+        return arrItem[0].type;
+    }
+}
+
+function removeChosenRows(){
+    $('.btnRemove').on("click", function(){
+        $('input:checkbox:checked').parents("tr").remove();
+    });
+}
+
+function gatherData(tempItems){
+    var tempInputsOfRow = $('tr:last > td').children('input');
+    var dataInRow = {};
+    $(tempInputsOfRow).each(function(index,element){
+        var classDataInRow = $(element).attr('class');
+        dataInRow[classDataInRow] = $(element).val();
+    });
+    tempItems.push(dataInRow);
+}
+
+function checkData(){
+    var dataInRow = {};
+    var tempInputsInRow = $('tr:last > td').children('input');
+    $(tempInputsInRow).each(function(index,element){
+        dataInRow[$(element).attr('class')] = _.findWhere(items, {type: $(element).attr('class'), label:  $(element).val(),
+            id: $(element).data('id')});
+    });
+    if(dataInRow['material'].pid != dataInRow['brand'].id){
+        alert("Неверный ввод производителя или материала.");
+        return false;
+    }
+    else{
+        if(dataInRow['brand'].pid != dataInRow['category'].id){
+            alert("Неверный ввод производителя или категории.");
+            return false;
+        }
+    }
+    return true;
+}
+
+function getTotal(){
+    var tempInputsOfRow = $('tr > td').children('input.cost');
+    var total = 0
+    $(tempInputsOfRow).each(function(index,element){
+        total += $(element.id)
+    })
+}
+
+//var arrItemSummary=[];
+//
+//
+var tempItems = [];
+
+var tempItem = {};
 
 var items = [
     {
